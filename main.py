d9f2df7a2231ab5d0649a4252d970f6f9e0c211d
Aashish Tangnami <aashishtangnami@Aashishs-MacBook-Air.local>
2022-11-25 14:38:19 -0500

    initial commit dash-plotly

---
+++ main.py
@@ -0,0 +1,10 @@
+from dash import Dash, html
+
+def main() -> None:
+    app = Dash()
+    app.title = "Any-Data EDA"
+    app.layout = html.Div()
+    app.run()
+
+if __name__ == '__main__':
+    main()
