45305f58781002557c8d5b98b12b4c2ac1cb04c3
Aashish Tangnami <aashishtangnami@Aashishs-MacBook-Air.local>
2022-11-25 14:48:28 -0500

    components

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 from dash import Dash, html
 
+
 def main() -> None:
     app = Dash()
     app.title = "Any-Data EDA"
     app.layout = html.Div()
     app.run()
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
